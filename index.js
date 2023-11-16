a2983c2c54e172b5255b99d197400191b5f28907
SlideMovies <87663026+SlideMovies@users.noreply.github.com>
2023-11-16 18:41:08 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    console.log(`Just got a request at ${req.url}!`)
-    res.write('Yo!');
-    res.end();
-}).listen(process.env.PORT || 3000);
+const GLOBAL_AGENT = require("global-agent");
+GLOBAL_AGENT.HTTP_PROXY = 'http://110.78.138.125:8080';
+
+
+// Now require cors-anywhere
+const corsAnywhere = require('cors-anywhere');
+
+// Set the CORS Anywhere server to use the global proxy
+corsAnywhere.createServer({
+  originWhitelist: []
+}).listen(8888, '0.0.0.0', function() {
+  console.log('CORS Anywhere is listening on port 8080');
+});
